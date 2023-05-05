c4c3ec0e1264dfb97a463cbf2e5f15740f8e1a26
juno-junho <ssmm0205@naver.com>
2023-05-06 01:45:54 +0900

    feat(TDD): 1장. 다중 통화를 지원하는 Money 객체

---
+++ test-driven-development/src/main/java/org/example/tdd/Dollar.java
@@ -0,0 +1,12 @@
+package org.example.tdd;
+
+public class Dollar {
+    int amount;
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public void times(int multiplier) {
+        amount *= multiplier;
+    }
+}
--- test-driven-development/src/test/java/org/example/tdd/MoneyTest.java
+++ test-driven-development/src/test/java/org/example/tdd/MoneyTest.java
@@ -1,2 +1,16 @@
 package org.example.tdd;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MoneyTest {
+
+    @Test
+    void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertThat(five.amount).isEqualTo(10);
+    }
+}
