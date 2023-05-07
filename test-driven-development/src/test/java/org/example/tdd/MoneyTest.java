225835ada303109a6312392cae7afa448bd9841a
juno-junho <ssmm0205@naver.com>
2023-05-07 23:50:18 +0900

    feat(TDD): 2장. 타락한 객체

--- test-driven-development/src/test/java/org/example/tdd/MoneyTest.java
+++ test-driven-development/src/test/java/org/example/tdd/MoneyTest.java
@@ -1,16 +1,26 @@
 package org.example.tdd;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MoneyTest {
 
+    // 1장
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
         five.times(2);
         assertThat(five.amount).isEqualTo(10);
+    }
+
+    // 2장
+    @Test
+    void testMultiplication2() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertThat(product.amount).isEqualTo(10);
+        product = five.times(3);
+        assertThat(product.amount).isEqualTo(15);
     }
 }
