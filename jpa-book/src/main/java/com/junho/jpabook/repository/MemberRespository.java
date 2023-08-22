e601e9c54958677c04328928f024f9a912b69b10
junho <ssmm0205@naver.com>
2023-08-22 19:28:20 +0900

    feat: native query 방식 추가

--- jpa-book/src/main/java/com/junho/jpabook/repository/MemberRespository.java
+++ jpa-book/src/main/java/com/junho/jpabook/repository/MemberRespository.java
@@ -3,6 +3,7 @@ package com.junho.jpabook.repository;
 import com.junho.jpabook.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
@@ -11,5 +12,13 @@ public interface MemberRespository extends JpaRepository<Member, Long> {
 
     // NamedQuery
     List<Member> findByUsername(@Param("username") String username);
+
+    // using Query annotation with JPQL
+    @Query("select m from Member m where m.username = ?1")
+    Member findByUsernameUsingQuery(String username);
+
+    // using Query annotation with native query
+    @Query(value = "select * from Member WHERE username = ?0", nativeQuery = true)
+    Member findByUsernameUsingNativeQuery(String username);
 
 }
