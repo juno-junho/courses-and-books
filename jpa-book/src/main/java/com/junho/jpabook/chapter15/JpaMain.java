1929d9ce86a25afae5ff21ad9180f85ca370a32d
junho <ssmm0205@naver.com>
2023-08-28 18:45:39 +0900

    feat: 페이징 배치 처리를 통한 영속성 컨텍스트 초기화

--- jpa-book/src/main/java/com/junho/jpabook/chapter15/JpaMain.java
+++ jpa-book/src/main/java/com/junho/jpabook/chapter15/JpaMain.java
@@ -29,4 +29,30 @@ public class JpaMain {
         tx.commit();
         em.close();
     }
+
+    public void usingPageingBatchQuery() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        int pageSize = 100;
+        for (int i = 0; i < 10; i++) {
+            List<Book> resultList = em.createQuery("select p from Book p", Book.class)
+                    .setFirstResult(i * pageSize)
+                    .setMaxResults(pageSize)
+                    .getResultList();
+
+            // 비즈니스 로직 실행
+            for (Book book : resultList) {
+                book.setIsbn(String.valueOf(book.getId() + 10000));
+            }
+            em.flush();
+            em.clear();
+        }
+        tx.commit();
+        em.close();
+    }
 }
