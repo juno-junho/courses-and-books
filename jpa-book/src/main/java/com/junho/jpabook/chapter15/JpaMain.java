e3cb041ddb15db49b99509f8e62fde5b5c311382
junho <ssmm0205@naver.com>
2023-08-28 18:52:57 +0900

    feat: hibernate session을 이용한 배치 최적화

--- jpa-book/src/main/java/com/junho/jpabook/chapter15/JpaMain.java
+++ jpa-book/src/main/java/com/junho/jpabook/chapter15/JpaMain.java
@@ -4,6 +4,10 @@ import com.junho.jpabook.chapter15.visitorpattern.Book;
 import com.junho.jpabook.chapter15.visitorpattern.Item;
 import com.junho.jpabook.entity.Member;
 import jakarta.persistence.*;
+import org.hibernate.CacheMode;
+import org.hibernate.ScrollMode;
+import org.hibernate.ScrollableResults;
+import org.hibernate.Session;
 
 import java.util.List;
 
@@ -54,5 +58,35 @@ public class JpaMain {
         }
         tx.commit();
         em.close();
+    }
+
+    public void usingHibernateScroll() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        Session session = em.unwrap(Session.class);
+        tx.begin();
+
+        ScrollableResults<Book> scroll = session.createQuery("select p from Book p", Book.class)
+                .setCacheMode(CacheMode.IGNORE) // 2차캐시 쓰기
+                .scroll(ScrollMode.FORWARD_ONLY);
+
+        int count = 0;
+
+        while (scroll.next()) {
+            Book book = scroll.get();
+            book.setIsbn(String.valueOf(book.getId() + 10000));
+
+            count++;
+            if (count % 100 == 0) {
+                session.flush(); // 플러시
+                session.clear(); // 영속성 컨텍스트 초기화
+            }
+        }
+
+        tx.commit();
+        session.close();
     }
 }
